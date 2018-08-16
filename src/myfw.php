8e3c1f86fb0fa5873be0d0d22b48e815cfe807f5
Francisco Azevedo <francisco.azevedo@advertbrands.com>
2018-08-16 23:39:22 +0100

    porting

--- src/myfw.php
+++ src/myfw.php
@@ -252,6 +252,11 @@ class myfw{
                 'Too much requests. Please wait ' . $container->memcached->ratelocktimeout() . 's and try again.');
         }
 
+        if ( isset( $container->config[ 'app.cidr' ] ) && !empty( $container->config[ 'app.cidr' ] ) && !$container->list->ip_in_rangelist( $_SERVER['REMOTE_ADDR'], $container->config[ 'app.cidr' ] ) ) {
+            throw new myexception(myexception::STOP,
+                'IP ' . $_SERVER['REMOTE_ADDR'] . ' not in APP cidr whitelist.');
+        }
+
         // myconfirm method to populate $_POST
         $xconfirm = $request->getHeaderLine('X-Confirm');
 
@@ -283,3 +288,4 @@ class myfw{
     function d($x){
         die(var_export($x));
     }
+
