74b64b04d45b2476ce161988f3175c13486a1c46
Francisco Azevedo <francisco.azevedo@advertbrands.com>
2018-09-05 15:09:40 +0100

    porting

--- src/mysession.php
+++ src/mysession.php
@@ -6,4 +6,13 @@ class mysession extends Slim\Middleware\Session{
     public function start(){
         $this->startSession();
     }
+
+    public function check(){
+
+        if( !isset( $_SESSION[ 'useragent' ] ) )
+            $_SESSION[ 'useragent' ] = $_SERVER[ 'HTTP_USER_AGENT' ];
+
+        if( $_SESSION[ 'useragent' ] !== $_SERVER[ 'HTTP_USER_AGENT' ] )
+            session_destroy();
+    }
 }
